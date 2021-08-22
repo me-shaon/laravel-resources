07adaac4e0d42e349aa3b7c809ca205e0a21f9d8
Ahmed Shamim <shaon.cse81@gmail.com>
2021-08-22 22:41:14 +0600

    fix: History and not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Info from '../views/Info.vue'
 import Official from '../views/Official.vue'
@@ -6,6 +6,7 @@ import Books from '../views/Books.vue'
 import Courses from '../views/Courses.vue'
 import Blogs from '../views/Blogs.vue'
 import People from '../views/People.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -42,11 +43,15 @@ const routes = [
     path: '/people',
     name: 'People',
     component: People
+  },
+  { 
+    path: '/:pathMatch(.*)',
+    component: NotFound 
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mx-auto mt-20 text-center text-white">
+        <h1 class="text-3xl mb-4">404</h1>
+        <h2 class="text-xl">You are lost. Please go back.</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
